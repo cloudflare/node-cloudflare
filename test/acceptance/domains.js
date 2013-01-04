7a3d7d9981dccf1dce41f0ad8c1aa186dc9b6e34
Justin Rainbow <justin.rainbow@gmail.com>
2013-01-04 13:24:23 -0700

    A code reshuffle

--- test/acceptance/domains.js
+++ test/acceptance/domains.js
@@ -5,54 +5,54 @@ var CF_TOKEN = 'abcdeF';
 var CF_EMAIL = 'my@email.tld';
 
 describe('CloudFlare', function () {
-    var client, server;
+  var client, server;
 
-    before(function (done) {
-        client = require('../../index').createClient({
-            token: CF_TOKEN,
-            email: CF_EMAIL
-        });
-        client.endpoint = 'http://localhost:5555';
-
-        server = require('../testServer').start(done);
+  before(function (done) {
+    client = require('../../').createClient({
+      token: CF_TOKEN,
+      email: CF_EMAIL
     });
+    client.endpoint = 'http://localhost:5555';
 
-    after(function (done) {
-        server.close();
+    server = require('../testServer').start(done);
+  });
+
+  after(function (done) {
+    server.close();
+    done();
+  });
+
+  describe('#listDomains', function () {
+
+    it('should require only the action parameter', function (done) {
+      server.expect('zone_load_multi');
+
+      client.listDomains(function (err, domains) {
+        assert.equal(err, null);
+
+        domains.should.have.length(3);
+        domains.getCount().should.equal(3);
+        domains.hasMore().should.not.be.ok;
+
         done();
+      });
     });
 
-    describe('#listDomains', function () {
+  });
 
-        it('should require only the action parameter', function (done) {
-            server.expect('zone_load_multi');
+  describe('#listDomainRecords', function () {
+    it('should process domain records', function (done) {
+      server.expect('rec_load_all');
 
-            client.listDomains(function (err, domains) {
-                assert.equal(err, null);
+      client.listDomainRecords('example.com', function (err, records) {
+        assert.equal(err, null);
 
-                domains.should.have.length(3);
-                domains.getCount().should.equal(3);
-                domains.hasMore().should.not.be.ok;
+        records.should.have.length(7);
+        assert.equal(7, records.getCount());
+        records.hasMore().should.not.be.ok;
 
-                done();
-            });
-        });
-
+        done();
+      });
     });
-
-    describe('#listDomainRecords', function () {
-        it('should process domain records', function (done) {
-            server.expect('rec_load_all');
-
-            client.listDomainRecords('example.com', function (err, records) {
-                assert.equal(err, null);
-
-                records.should.have.length(7);
-                assert.equal(7, records.getCount());
-                records.hasMore().should.not.be.ok;
-
-                done();
-            });
-        });
-    });
+  });
 });
--- test/acceptance/requestData.js
+++ test/acceptance/requestData.js
@@ -4,32 +4,31 @@ var CF_TOKEN = 'abcdeF';
 var CF_EMAIL = 'my@email.tld';
 
 describe('CloudFlare', function () {
-    var client = require('../../index').createClient({
-        token: CF_TOKEN,
-        email: CF_EMAIL
+  var client = require('../../').createClient({
+    token: CF_TOKEN,
+    email: CF_EMAIL
+  });
+
+  describe('#createRequestData', function () {
+
+    it('should require only the action parameter', function () {
+      var data = client.createRequestData('zone_load_multi');
+
+      data.should.be.a('object').and.include({
+        tkn: CF_TOKEN,
+        email: CF_EMAIL,
+        a: 'zone_load_multi'
+      });
     });
 
-    describe('#createRequestData', function () {
+    it('should merge additional parameters', function () {
+      var data = client.createRequestData('zone_load_multi', { z: "example.com" });
 
-        it('should require only the action parameter', function () {
-            var data = client.createRequestData('zone_load_multi');
-
-            data.should.be.a('object').and.include({
-                tkn: CF_TOKEN,
-                email: CF_EMAIL,
-                a: 'zone_load_multi'
-            });
-        });
-
-        it('should merge additional parameters', function () {
-            var data = client.createRequestData('zone_load_multi', { z: "example.com" });
-
-            data.should.be.a('object').and.include({
-                tkn: CF_TOKEN,
-                email: CF_EMAIL,
-                z: "example.com"
-            });
-        });
-
+      data.should.be.a('object').and.include({
+        tkn: CF_TOKEN,
+        email: CF_EMAIL,
+        z: "example.com"
+      });
     });
+  });
 });
