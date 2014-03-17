ee380306fd09de10d9e5df9508bda4af64b31403
Justin Rainbow <justin.rainbow@gmail.com>
2014-03-17 08:51:41 -0700

    Fixes requestData spec

--- test/acceptance/requestData.js
+++ test/acceptance/requestData.js
@@ -14,7 +14,7 @@ describe('CloudFlare', function () {
     it('should require only the action parameter', function () {
       var data = client.createRequestData('zone_load_multi');
 
-      data.should.be.a('object').and.include({
+      data.should.be.an.instanceof(Object).and.include({
         tkn: CF_TOKEN,
         email: CF_EMAIL,
         a: 'zone_load_multi'
@@ -24,7 +24,7 @@ describe('CloudFlare', function () {
     it('should merge additional parameters', function () {
       var data = client.createRequestData('zone_load_multi', { z: "example.com" });
 
-      data.should.be.a('object').and.include({
+      data.should.be.an.instanceof(Object).and.include({
         tkn: CF_TOKEN,
         email: CF_EMAIL,
         z: "example.com"
