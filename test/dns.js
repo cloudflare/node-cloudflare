f645ec01388721507701b88c7d423a4a6585b189
matiboy <mathieu.chauvinc@gmail.com>
2016-12-22 06:06:30 +0800

    Remove tests that used Sinon

--- test/dns.js
+++ test/dns.js
@@ -1,7 +1,6 @@
 /* eslint-disable camelcase */
 import test from 'ava';
 import nock from 'nock';
-import sinon from 'sinon';
 import CF, {DNSRecord, PaginatedResponse, Zone} from '../';
 
 nock.disableNetConnect();
@@ -247,28 +246,6 @@ test('create SRV record', async t => {
   t.true(record.data.name === 'a');
 });
 
-test('passes only type and data for SRV record creation', async t => {
-  const rr = DNSRecord.create({
-    zone_id: '1',
-    type: 'SRV',
-    name: 'something',
-    content: 'random content',
-    data: {
-      name: 'a'
-    }
-  });
-  const cf = t.context.cf;
-  const fakeGot = sinon.stub(cf, '_got').returns(Promise.resolve({
-    body: {
-      result: rr.toJSON()
-    }
-  }));
-  await t.context.cf.addDNS(rr);
-  const body = JSON.parse(fakeGot.getCall(0).args[1].body);
-  // Should only have type and data as keys, even though more data was passed to DNSRecord constructor
-  t.deepEqual(Object.keys(body).sort(), ['data', 'type'], 'Incorrect values present in body');
-});
-
 test('create MX record without priority - defaults to 0', async t => {
   const rr = DNSRecord.create({
     zone_id: '1',
@@ -289,26 +266,6 @@ test('create MX record without priority - defaults to 0', async t => {
   t.is(record.priority, 0);
 });
 
-test('creating MX record includes priority in body', async t => {
-  const rr = DNSRecord.create({
-    zone_id: '1',
-    type: 'MX',
-    name: 'something.com',
-    ttl: 100
-  });
-  const cf = t.context.cf;
-  const fakeGot = sinon.stub(cf, '_got').returns(Promise.resolve({
-    body: {
-      result: rr.toJSON()
-    }
-  }));
-  await t.context.cf.addDNS(rr);
-  // Re-read body from the _got call
-  const body = JSON.parse(fakeGot.getCall(0).args[1].body);
-  // Should include priority even if not provided in rr
-  t.is(body.priority, 0);
-});
-
 test('create MX record with priority', async t => {
   const rr = DNSRecord.create({
     zone_id: '1',
@@ -317,15 +274,15 @@ test('create MX record with priority', async t => {
     ttl: 100,
     priority: 50
   });
-  const cf = t.context.cf;
-  const fakeGot = sinon.stub(cf, '_got').returns(Promise.resolve({
-    body: {
-      result: rr.toJSON()
+  nock('https://api.cloudflare.com')
+  .post('/client/v4/zones/1/dns_records')
+  .reply(200, {
+    result: {
+      id: '12345',
+      type: 'MX',
+      priority: rr.priority
     }
-  }));
-  await t.context.cf.addDNS(rr);
-  // Re-read body from the _got call
-  const body = JSON.parse(fakeGot.getCall(0).args[1].body);
-  // Should have the priority provided to rr
-  t.is(body.priority, 50);
+  });
+  let record = await t.context.cf.addDNS(rr);
+  t.is(record.priority, 50);
 });
