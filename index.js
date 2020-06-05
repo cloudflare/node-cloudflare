5ab3c108f8e63486cf3608e08adea10c9d989e01
Perlover <perlover@perlover.com>
2020-06-05 23:07:13 +0200

    The page rules instance (not tested yet)

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const resources = {
   enterpriseZoneWorkersKVNamespaces: require('./lib/resources/EnterpriseZoneWorkersKVNamespaces'),
   enterpriseZoneWorkersKV: require('./lib/resources/EnterpriseZoneWorkersKV'),
   ips: require('./lib/resources/IPs'),
+  pageRules: require('./lib/resources/PageRules'),
   zones: require('./lib/resources/Zones'),
   zoneSettings: require('./lib/resources/ZoneSettings'),
   zoneCustomHostNames: require('./lib/resources/ZoneCustomHostNames'),
@@ -67,6 +68,7 @@ const withEnvProxy = function withEnvProxy(opts) {
  *
  * @property {DNSRecords} dnsRecords - DNS Records instance
  * @property {IPs} ips - IPs instance
+ * @property {PageRules} pageRules - Page Rules instance
  * @property {Zones} zones - Zones instance
  * @property {ZoneSettings} zoneSettings - Zone Settings instance
  * @property {ZoneCustomHostNames} zoneCustomHostNames - Zone Custom Host Names instance
